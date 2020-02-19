3ef99effc1a878d01b8c5afc3165a0fa480ca9cf
LLapuras <chenwanwan13@gmail.com>
2020-02-19 06:28:21 +0000

    add 2d sprite package

---
+++ LapuDevLibrary/Assets/Scripts/Countdown.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour
+{
+    public Text cdtime;
+    public int cd = 20;
+    IEnumerator WaitAndPrint()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(3);
+        print("WaitAndPrint " + Time.time);
+    }
+
+    IEnumerator Count()
+    {
+        while (cd >= 0)
+        {
+            yield return new WaitForSeconds(1);
+            cd--;
+        }
+    }
+
+    void Start()
+    {
+        StartCoroutine(Count());
+    }
+
+    void Update()
+    {
+        cdtime.text = cd.ToString();
+        if (cd == 0)
+        {
+            Debug.Log("done");
+
+        }
+    }
+}
